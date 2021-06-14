7716ef52c9b0c2c0d770855af7592b0b14aa5b5f
McCambley <jakemccamb16@gmail.com>
2021-06-13 23:25:43 -0400

    Dev: scroll behavior on tablet screens

--- index.js
+++ index.js
@@ -1,18 +1,70 @@
+// HEADER FUNCTIONALITY
+
+const header = document.querySelector('.header');
 const hamburger = document.querySelector('.header__hamburger');
 const headerContainer = document.querySelector('.header__content');
 const logo = document.querySelector('.header__logo');
+const logoAccent = document.querySelector('.header__logo-accent');
+const headerLinks = Array.from(document.querySelectorAll('.header__link'));
+const hero = document.querySelector('.hero');
+const heroHeight = hero.offsetHeight;
 
+// open or close navigation on mobile layous
 function toggleNav() {
+  // check if the menu is open already
   const menuStatus = headerContainer.classList.contains('header__content_open');
   if (!menuStatus) {
+    // if menu is not open
+    // extend overlay to device height
+    header.classList.add('header_open');
+    // change hamburger icon
     hamburger.src = './images/hamburger-close.svg';
+    // extend nav container to display hidden links
     headerContainer.classList.add('header__content_open');
+    // change logo color
     logo.classList.add('header__logo_opened');
+    // listen for click on overlay outside of open nav menu
+    header.addEventListener('click', toggleNavOnOverlay);
   } else {
+    // if menu is open already, undo the above
+    header.classList.remove('header_open');
     hamburger.src = './images/hamburger-open.svg';
     headerContainer.classList.remove('header__content_open');
     logo.classList.remove('header__logo_opened');
+    header.removeEventListener('click', toggleNavOnOverlay);
+  }
+}
+
+// recolor menu items when user scrolls past hero
+function recolorScrolledMenu() {
+  // if window is wider than the first desktop breakpoint
+  // and the user has scrolled beyond the hero block
+  if (window.innerWidth > 0 && window.scrollY + 100 > heroHeight) {
+    headerContainer.classList.add('header__content_scrolled');
+    logo.classList.add('header__logo_scrolled');
+    logoAccent.classList.add('header__logo-accent_scrolled');
+    headerLinks.forEach(link => {
+      link.classList.add('header__link_scrolled');
+    });
+  } else {
+    headerContainer.classList.remove('header__content_scrolled');
+    logo.classList.remove('header__logo_scrolled');
+    logoAccent.classList.remove('header__logo-accent_scrolled');
+    headerLinks.forEach(link => {
+      link.classList.remove('header__link_scrolled');
+    });
+  }
+}
+
+// toggle menuStatus on overlay click
+function toggleNavOnOverlay(e) {
+  if (e.target.classList.contains('header_open')) {
+    toggleNav();
   }
 }
 
 hamburger.addEventListener('click', toggleNav);
+window.addEventListener('scroll', recolorScrolledMenu);
+window.addEventListener('resize', recolorScrolledMenu);
+
+// HERO FUNCTIONALITY
